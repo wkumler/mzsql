12ad865f22065f8f1ce71539826d005785866b58
William Kumler <43116923+wkumler@users.noreply.github.com>
2024-12-05 17:19:43 -0800

    Moved mzDB RT hotfix to src

--- tests/test_mzdb_functions.py
+++ tests/test_mzdb_functions.py
@@ -6,7 +6,6 @@ from mzsql import *
 def test_get_chrom_mzdb():
     ref_data = get_chrom_mzml_pyteomics("demo_data/180205_Poo_TruePoo_Full1.mzML", 118.0865, 10)
     test_data = get_chrom_mzdb("demo_data/180205_Poo_TruePoo_Full1.raw.mzDB", 118.0865, 10)
-    test_data["rt"] /= 60  # Adjust rt for mzdb
     assert ref_data["rt"].equals(test_data["rt"])
     assert ref_data["mz"].equals(test_data["mz"])
     assert ref_data["int"].equals(test_data["int"])
