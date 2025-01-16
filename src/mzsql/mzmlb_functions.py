d7e5a8342b897aa28a4daf7eb5d0e6d1c09268f0
wkumler <wkumler@uw.edu>
2025-01-15 16:31:22 -0800

    Adding mzmlb MS2 things

--- src/mzsql/mzmlb_functions.py
+++ src/mzsql/mzmlb_functions.py
@@ -68,3 +68,51 @@ def get_rtrange_mzmlb(file, rtstart, rtend):
             df_scan = pd.DataFrame({'mz':mz_vals, 'int':int_vals, 'rt':[rt_val]*len(mz_vals)})
             scan_dfs.append(df_scan)
     return(pd.concat(scan_dfs, ignore_index=True))
+
+def get_MS2scan_mzmlb(mzmlb_file, scan_num):
+    file_data = pyteomics.mzmlb.MzMLb(mzmlb_file)
+    rt_val = file_data[scan_num]['scanList']['scan'][0]['scan start time']
+    premz_val = file_data[scan_num]["precursorList"]["precursor"][0]["isolationWindow"]['isolation window target m/z']
+    return(pd.DataFrame({"rt":rt_val, "premz":premz_val, "fragmz":file_data[scan_num]['m/z array'], "int":file_data[scan_num]['intensity array']}))
+
+def get_MS2premz_mzmlb(mzmlb_file, precursor_mz, ppm):
+    mzmin, mzmax = pmppm(precursor_mz, ppm_acc)
+    scan_dfs = []
+    for spectrum in pyteomics.mzmlb.MzMLb(mzmlb_file):
+        if spectrum['ms level'] == 2:
+            rt_val = spectrum['scanList']['scan'][0]['scan start time']
+            premz_val = spectrum["precursorList"]["precursor"][0]["isolationWindow"]['isolation window target m/z']
+            if mzmin < premz_val < mzmax:
+                mz_vals=spectrum['m/z array']
+                int_vals = spectrum['intensity array']
+                df_scan = pd.DataFrame({'rt':rt_val, 'premz':premz_val, 'fragmz':mz_vals, 'int':int_vals})
+                scan_dfs.append(df_scan)
+    return(pd.concat(scan_dfs, ignore_index=True))
+
+def get_MS2fragmz_mzmlb(mzmlb_file, fragment_mz, ppm):
+    mzmin, mzmax = pmppm(fragment_mz, ppm_acc)
+    scan_dfs = []
+    for spectrum in pyteomics.mzmlb.MzMLb(mzmlb_file):
+        if spectrum['ms level'] == 2:
+            rt_val = spectrum['scanList']['scan'][0]['scan start time']
+            premz_val = spectrum["precursorList"]["precursor"][0]["isolationWindow"]['isolation window target m/z']
+            chosen_frag_idxs = (mzmin < spectrum['m/z array']) & (spectrum['m/z array'] < mzmax)
+            mz_vals=spectrum['m/z array'][chosen_frag_idxs]
+            int_vals = spectrum['intensity array'][chosen_frag_idxs]
+            df_scan = pd.DataFrame({'rt':rt_val, 'premz':premz_val, 'fragmz':mz_vals, 'int':int_vals})
+            scan_dfs.append(df_scan)
+    return(pd.concat(scan_dfs, ignore_index=True))
+
+def get_MS2nloss_mzmlb(mzmlb_file, neutral_loss, ppm):
+    mzmin, mzmax = pmppm(neutral_loss, ppm_acc)
+    scan_dfs = []
+    for spectrum in pyteomics.mzmlb.MzMLb(mzmlb_file):
+        if spectrum['ms level'] == 2:
+            rt_val = spectrum['scanList']['scan'][0]['scan start time']
+            premz_val = spectrum["precursorList"]["precursor"][0]["isolationWindow"]['isolation window target m/z']
+            chosen_frag_idxs = (mzmin < premz_val-spectrum['m/z array']) & (premz_val-spectrum['m/z array'] < mzmax)
+            mz_vals=spectrum['m/z array'][chosen_frag_idxs]
+            int_vals = spectrum['intensity array'][chosen_frag_idxs]
+            df_scan = pd.DataFrame({'rt':rt_val, 'premz':premz_val, 'fragmz':mz_vals, 'int':int_vals})
+            scan_dfs.append(df_scan)
+    return(pd.concat(scan_dfs, ignore_index=True))
