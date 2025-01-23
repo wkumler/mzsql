9240f3c993a867a14a5af6d11b51a5998948629b
wkumler <wkumler@uw.edu>
2025-01-23 12:10:04 -0800

    Removing autogenerated docstrings for duckdb functions

--- src/mzsql/duckdb_functions.py
+++ src/mzsql/duckdb_functions.py
@@ -6,17 +6,6 @@ import pyteomics.mzml
 from .helpers import pmppm
 
 def turn_mzml_duckdb(file, outfile, ordered=None):
-    """
-    Converts an mzML file into a DuckDB database table.
-    
-    Args:
-        file (str): Path to the mzML file to be processed.
-        outfile (str): Path to the output DuckDB file where the data will be stored.
-        ordered (bool, optional): If True, the mz values are sorted before storing. Defaults to False.
-    
-    Returns:
-        str: The path to the output DuckDB file.
-    """
     conn = duckdb.connect(outfile)
     conn.execute("DROP TABLE IF EXISTS MS1")
 
@@ -53,17 +42,6 @@ def turn_mzml_duckdb(file, outfile, ordered=None):
     return outfile
 
 def get_chrom_duckdb(file, mz, ppm):
-    """
-    Retrieves chromatogram data from the DuckDB database for a specified m/z range.
-
-    Args:
-        file (str): Path to the DuckDB file.
-        mz (float): The target m/z value.
-        ppm (float): The allowed mass tolerance in parts per million (ppm).
-
-    Returns:
-        pd.DataFrame: A DataFrame containing m/z, intensity, and retention time data.
-    """
     conn = duckdb.connect(file)
     mz_min, mz_max = pmppm(mz, ppm)
     query = "SELECT mz, int, rt FROM MS1 WHERE mz BETWEEN ? AND ?"
@@ -72,16 +50,6 @@ def get_chrom_duckdb(file, mz, ppm):
     return query_data
 
 def get_spec_duckdb(file, spectrum_idx):
-    """
-    Retrieves the spectrum data for a specific scan ID from the DuckDB database.
-
-    Args:
-        file (str): Path to the DuckDB file.
-        spectrum_idx (int): The scan ID of the spectrum to retrieve.
-
-    Returns:
-        pd.DataFrame: A DataFrame containing all columns for the specified spectrum.
-    """
     conn = duckdb.connect(file)
     query = "SELECT * FROM MS1 WHERE id = ?"
     spectrum_data = conn.execute(query, (spectrum_idx,)).fetchdf()
@@ -89,23 +57,11 @@ def get_spec_duckdb(file, spectrum_idx):
     return spectrum_data
 
 def get_rtrange_duckdb(file, rtstart, rtend):
-    """
-    Retrieves spectrum data within a specified retention time range from the DuckDB database.
-
-    Args:
-        file (str): Path to the DuckDB file.
-        rtstart (float): The start retention time value.
-        rtend (float): The end retention time value.
-
-    Returns:
-        pd.DataFrame: A DataFrame containing all columns for spectra within the specified retention time range.
-    """
     conn = duckdb.connect(file)
     query = "SELECT * FROM MS1 WHERE rt BETWEEN ? AND ?"
     rt_range_data = conn.execute(query, (rtstart, rtend)).fetchdf()
     conn.close()
     return rt_range_data
-
 
 
 def get_MS2scan_duckdb(file, spectrum_idx):
